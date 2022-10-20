b0b402d62ee61c8ffab7a1a11860a37016d11f6e
Galacoder <galacoder69@gmail.com>
2022-10-20 22:06:04 +0700

    remove supabase secrets

--- packages/app/lib/supabase.ts
+++ packages/app/lib/supabase.ts
@@ -1,9 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://smjvcizkaxscoqgignso.supabase.co'
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtanZjaXprYXhzY29xZ2lnbnNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTA5NDI1MjYsImV4cCI6MTk2NjUxODUyNn0.FxGV0933y8j5Wca4sQlBWeSLlTczvCLc-5lYBzR_wU8'
+const supabaseUrl = ''
+const supabaseAnonKey = ''
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
